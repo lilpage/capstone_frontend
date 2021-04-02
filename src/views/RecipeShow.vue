811ac3f3e0a6e48985e329305604f9c9aaf42869
Lil <lil@Lils-MacBook-Air.local>
2021-04-02 15:45:45 -0500

    Adds User and Show vues

--- src/views/RecipeShow.vue
+++ src/views/RecipeShow.vue
@@ -1,5 +1,39 @@
 <template>
   <div class="recipe-show">
-    <h1>This is a recipe</h1>
+    <h2>{{ recipe.name }}</h2>
+    <div>Ingredients: {{ recipe.ingredients }}</div>
+    <div>Directions: {{ recipe.directions }}</div>
+    <!-- Favorite  -->
+    <button v-on:click="addToFavorites()">favorite</button>
   </div>
 </template>
+
+<style>
+</style>
+
+<script>
+import axios from "axios";
+
+export default {
+  data: function () {
+    return {
+      recipe: {},
+    };
+  },
+  created: function () {
+    this.recipeShow();
+  },
+  methods: {
+    recipeShow: function () {
+      axios.get("/api/recipes/" + this.$route.params.id).then((response) => {
+        console.log(response.data);
+        this.recipe = response.data;
+      });
+    },
+    addToFavorites: function () {
+      console.log("added to favorites");
+      // ?????
+    },
+  },
+};
+</script>
--- src/views/User.vue
+++ src/views/User.vue
@@ -1,5 +1,28 @@
 <template>
   <div class="user">
     <h1>This is your user page</h1>
+    {{ favorites }}
   </div>
 </template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data: function () {
+    return {
+      favorites: [],
+    };
+  },
+  created: function () {
+    this.favoritesShow();
+  },
+  methods: {
+    favoritesShow: function () {
+      axios.get("/api/favorites").then((response) => {
+        console.log(response.data);
+      });
+    },
+  },
+};
+</script>
