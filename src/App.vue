1f45320469a31e84b35f1877d37c3442aeaafe4c
Lil <lillehpage@gmail.com>
2021-04-28 15:54:44 -0500

    Footer update

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,8 @@
     <nav class="navbar navbar-expand-lg fixed-top navbar-custom sticky sticky-dark">
         <div class="container">
             <!-- LOGO -->
-            <a class="navbar-brand logo text-uppercase" href="/">
-                <p id="db-logo">Drinking Buddy</p>
+            <a  href="/">
+                <h1 id="db-logo">Drinking Buddy</h1>
             </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse"
                 aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
@@ -44,83 +44,12 @@
       <section class="section bg-light bg-footer pb-5">
         <div class="container">
             <div class="row">
-                <div class="col-lg-4">
-                    <div class="footer-info mt-4">
-                        <p id="db-logo">Drinking Buddy</p>
-                        <!-- <div class="team-social mt-4 pt-2">
-                            <ul class="list-inline mb-0">
-                                <li class="list-inline-item">
-                                    <a href="" class="text-reset"><i class="mdi mdi-facebook"></i></a>
-                                </li>
-                                <li class="list-inline-item">
-                                    <a href="" class="text-reset"><i class="mdi mdi-twitter"></i></a>
-                                </li>
-                                <li class="list-inline-item">
-                                    <a href="" class="text-reset"><i class="mdi mdi-google"></i></a>
-                                </li>
-                                <li class="list-inline-item">
-                                    <a href="" class="text-reset"><i class="mdi mdi-pinterest"></i></a>
-                                </li>
-                            </ul>
-                        </div> -->
-
-                    </div>
+                <div class="col-lg-6">
                 </div>
-
-                <!-- <div class="col-lg-4">
-                    <div class="row  pl-0 md-lg-5">
-                        <div class="col-lg-6">
-                            <div class="mt-4">
-                                <h5 class="f-20">Services</h5>
-                                <ul class="list-unstyled footer-link mt-3">
-                                    <li><a href="">Web Design</a></li>
-                                    <li><a href="">Lorem Test</a></li>
-                                    <li><a href="">Credit</a></li>
-                                    <li><a href="">Ui Design</a></li>
-                                </ul>
-                            </div>
-                        </div>
-
-                        <div class="col-lg-6">
-                            <div class="mt-4">
-                                <h5 class="f-20">Company</h5>
-                                <ul class="list-unstyled footer-link mt-3">
-                                    <li><a href="">Features</a></li>
-                                    <li><a href="">Faq</a></li>
-                                    <li><a href="">Contact us</a></li>
-                                </ul>
-                            </div>
-                        </div>
-
-                    </div>
-                </div>
-
-                <div class="col-lg-4">
-                    <div class="mt-4">
-                        <h5 class="f-20">Subscribe</h5>
-
-                        <div class="subscribe mt-4 pt-1">
-                            <form action="#">
-                                <input placeholder="Enter Email" type="text">
-                                <button type="submit" class="btn btn-primary"><i class="mdi mdi-send"></i></button>
-                            </form>
-                        </div>
-                    </div>
-                </div> -->
-
-            </div>
-
-            <hr class="my-5">
-
-            <div class="row">
-                <div class="col-12">
-                    <div class="text-center">
-                        <p class="text-muted mb-0">This is the copyright line</p>
-                    </div>
+                <div class="col-lg-6">
+                    <p> <a id="db-logo">Drinking Buddy</a> | made by Lil | 2021</p>
                 </div>
             </div>
-
-
         </div>
     </section>
   </div>
